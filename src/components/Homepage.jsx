97b3645a13b03344e5ae5bd05aa16cbe6bd205eb
Cindellai <52719448+Cindellai@users.noreply.github.com>
2024-02-13 20:45:46 -0600

    response hardcoded

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,17 +3,18 @@ import { useState } from "react";
 import icon from "../carRepair.png"
 import "./Homepage.css"
 import data from "../../data/data.json"
-import CodeInfoPage from "./CodeInfoPage";
+import "./response.css"
 
-const HomePage = ()  => {
+
+const HomePage = () => {
     const [code, setCode] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const newCode = e.target.code.value 
+        const newCode = e.target.code.value
         if (data[newCode]) {
             setCode(-1)
-        }else{
+        } else {
             setCode(e.target.code.value)
         }
     }
@@ -29,36 +30,76 @@ const HomePage = ()  => {
                 <p className="form-descriptor">To learn more about your car's required maintenance, input the repair code given by your OBD reader. </p>
                 <form onSubmit={handleSubmit}>
                     <div class="form-group">
-                        <input 
+                        <input
                             type="text"
-                            class="form-control" 
-                            name="code" 
-                            placeholder="Ex. P0100"/>
+                            class="form-control"
+                            name="code"
+                            placeholder="Ex. P0100" />
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
-             </div>
-            
+            </div>
             <div>
-                {code ? 
+                {code ?
                     <div>
-                        {code == -1 ? 
-                        <div>
-                            <h1>The code was not found :/</h1>
-                        </div>
-                        : 
-                        <div>
-                            <h1>More information for: {code}</h1>
-                        </div>
+                        {code == -1 ?
+                            <div>
+                                <h1>The code was not found :/</h1>
+                            </div>
+                            :
+                            <div>
+                                <div>
+                                    <ul>
+                                        <li>Description: Replace Catalytic Converter(s) with new OE Catalytic Converter(s)</li>
+                                        <li>Urgency: 2
+                                            <ul>
+                                                <li>Urgency Description: Repair immediately if drivability issues are present. Threat to essential system components if not repaired as soon as possible.</li>
+                                            </ul>
+                                        </li>
+                                        <li>Repair:
+                                            <ul>
+                                                <li>Difficulty: 3</li>
+                                                <li>Part Cost: $1967.01</li>
+                                                <li>Labor Cost: $244.674</li>
+                                                <li>Misc Cost: $25</li>
+                                                <li>Total Cost: $2236.684</li>
+                                            </ul>
+                                        </li>
+                                        <li>Parts:
+                                            <ul>
+                                                <li>Part: Catalytic Converter
+                                                    <ul>
+                                                        <li>Manufacturer: </li>
+                                                        <li>Price: $683.67</li>
+                                                        <li>Quantity: 1</li>
+                                                    </ul>
+                                                </li>
+                                                <li>Part: Transmission
+                                                    <ul>
+                                                        <li>Manufacturer: </li>
+                                                        <li>Price: $1283.34</li>
+                                                        <li>Quantity: 1</li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>TSB:
+                                            <ul>
+                                                <li>Instructions: <a href="http://downloads.innova.com/tsb-files/118000/4824780.pdf">Download TSB Instructions</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         }
                     </div>
-                    :
-                    <div></div>
-                }
-            </div>
-        </div>
 
-    );
-  }
-  
+            :
+            <div>
+                </div>
+
+}
+</div>
+</div> 
+    );}
   export default HomePage;
