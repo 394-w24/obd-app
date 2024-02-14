af7f39a0debf13b7384eec26b6d40bf16c59400f
Macy Bosnich <macybosnich2024@u.northwestern.edu>
2024-02-13 20:18:23 -0600

    add vin and miles fields

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,14 +5,17 @@ import "./Homepage.css"
 import CodeInfoPage from "./CodeInfoPage";
 
 const HomePage = ()  => {
-    const [code, setCode] = useState({});
-
-    const handleInputChange = (event) => {
-        setCode(event.target.value.toUpperCase());
-      };
+    const [code, setCode] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const newCode = e.target.code.value 
+        console.log(newCode)
+        if (data[newCode]) {
+            setCode(-1)
+        }else{
+            setCode(e.target.code.value)
+        }
     }
     return (
         <div className="container">
@@ -23,18 +26,53 @@ const HomePage = ()  => {
                 </a>
             </div>
             <div className="inner">
-                <p className="form-descriptor">To learn more about your car's required maintenance, input the repair code given by your OBD reader. </p>
+                <p className="form-descriptor">To learn more about your car's required maintenance, input some information about you car and its DTC code. </p>
                 <form onSubmit={handleSubmit}>
                     <div class="form-group">
+                        <h4>VIN Number:</h4>
                         <input 
                             type="text"
                             class="form-control" 
-                            onChange={handleInputChange} 
+                            name="vin" 
+                            placeholder="Ex. 4Y1SL65848Z411439"/>
+                    </div>
+                    <div class="form-group">
+                        <h4>Car Mileage:</h4>
+                        <input 
+                            type="text"
+                            class="form-control" 
+                            name="miles" 
+                            placeholder="Ex. 45678"/>
+                    </div>
+                    <div class="form-group">
+                        <h4>DTC code:</h4>
+                        <input 
+                            type="text"
+                            class="form-control" 
+                            name="code" 
                             placeholder="Ex. P0100"/>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
              </div>
+            
+            <div>
+                {code ? 
+                    <div>
+                        {code == -1 ? 
+                        <div>
+                            <h1>The code was not found :/</h1>
+                        </div>
+                        : 
+                        <div>
+                            <h1>More information for: {code}</h1>
+                        </div>
+                        }
+                    </div>
+                    :
+                    <div></div>
+                }
+            </div>
         </div>
 
     );
