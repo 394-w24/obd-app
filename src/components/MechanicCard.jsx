2608653bbe071f1055c86326ae6454fb15063a5a
macybos <84352078+macybos@users.noreply.github.com>
2024-03-04 00:26:36 -0600

    view mechanics and request quote

--- src/components/MechanicCard.jsx
+++ src/components/MechanicCard.jsx
@@ -1,19 +1,57 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import "@fortawesome/fontawesome-free/css/all.css";
+import { Document, Page} from 'react-pdf';
 import "./MechanicCard.css";
 
-  const MechanicCard = ({id, tags}) => {
-    const name = tags.name
+  const MechanicCard = ({name, quote, rating}) => {
 
+    const [quoteReceived, setQuoteReceived] = useState()
+    const [viewQuote, setViewQuote] = useState()
+
+    const quoteRequested = () => {
+      setTimeout(function () {
+        console.log("timeout")
+      }, 10000)
+      setQuoteReceived(true)
+    }
+
+    console.log(quote)
+
+    const viewQuoteClicked = () => {
+      setViewQuote(true)
+    }
+
+    const closePopup = () => {
+      setViewQuote(false)
+    }
     return (
-      <div className="card" style={{ width: '18rem' }}>
+      <div className="card" style={{ width: '90%' }}>
         <div className="card-body">
           <div className="card-title">
-            {name}
+            <div>
+              {name}
+            </div>
+            <div>
+              {rating}/5.0 <i className="fa-solid fa-star" />
+            </div>
+          </div>
+          <div style={{justifyContent: "center"}}>
+          <button type="button" className="btn btn-primary" onClick={quoteRequested}>Request Quote</button>
+          {quoteReceived ? <button type="button" style = {{padding:"15px 35px", fontSize: "20px"}}className="btn btn-primary" onClick={viewQuoteClicked}>View Quote</button> : <button type="button" className="btn btn-primary" disabled>View Quote</button>}
           </div>
         </div>
+        {viewQuote && (
+                <div className="quote-popup">
+                    <div className="quote-popup-content">
+                        <span className="close-popup" onClick={closePopup}>&times;</span>
+                        <iframe src={quote}/>
+                    </div>
+                </div>
+            )}
       </div>
   );
 };
 
 export default MechanicCard;
+
