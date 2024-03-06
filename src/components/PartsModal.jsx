ebe225c5ad95e703a4f787c09a186b679d3b745c
Cindellai <52719448+Cindellai@users.noreply.github.com>
2024-03-05 23:04:10 -0600

    fetch images from firebase for parts location

--- src/components/PartsModal.jsx
+++ src/components/PartsModal.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import './PartsModal.css'; 
 
 const PartsModal = ({ isOpen, onClose }) => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [secondImageUrl, setSecondImageUrl] = useState('');
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const storage = getStorage();
+      const secondImageRef = ref(storage, 'gs://obd-app-acded.appspot.com/p1050part.webp');
+      const imageRef = ref(storage, 'gs://obd-app-acded.appspot.com/ExhuastSystem.jpeg');
+
+      try {
+        const firstUrl = await getDownloadURL(imageRef);
+        setImageUrl(firstUrl);
+        const secondUrl = await getDownloadURL(secondImageRef);
+        setSecondImageUrl(secondUrl);
+      } catch (error) {
+        console.error("Error fetching image URLs:", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
   if (!isOpen) return null;
 
   return (
@@ -9,12 +32,15 @@ const PartsModal = ({ isOpen, onClose }) => {
       <div className="parts-modal-content">
         <button className="parts-modal-close" onClick={onClose}>&times;</button>
         <h2 className="centered-text">Part Location</h2>
-        <img src={'src/assets/Bank2Sensor1.jpg'} alt="Parts Diagram" />
-        <li>Bank 1 is the side with cylinder number 1 (Cylinders 1 – 3 – 5 – 7 etc.)</li>
-        <li>Bank 2 is the side with cylinder number 2 (Cylinders 2 – 4 – 6 – 8 etc.)</li>
-        <hr></hr>
-        <p> The O2 sensors are mounted in the exhaust system of the car. Sensor 2 is mounted in the exhaust stream and is located after the catalytic convertor.</p>
-   
+        {imageUrl ? <img src={imageUrl} alt="Parts Diagram" /> : "Loading image..."}
+        <p className="centered-text"> Oxygen Sensor in the Exhaust system</p>
+        <hr />
+        {secondImageUrl ? <img src={secondImageUrl} alt="Second Parts Diagram" /> : "Loading second image..."}
+        <div className="text-box">
+          <li>Bank 1 is the side with cylinder number 1 (Cylinders 1 – 3 – 5 – 7 etc.)</li>
+          <li>Bank 2 is the side with cylinder number 2 (Cylinders 2 – 4 – 6 – 8 etc.)</li>
+        <p className="text"> The O2 sensors are mounted in the exhaust system of the car. Sensor 2 is mounted in the exhaust stream and is located after the catalytic converter.</p>
+        </div>
       </div>
     </div>
   );
