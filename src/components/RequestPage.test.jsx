c242a1921a3fd375c181facba3d50e99a4cf7ac0
eraj14 <115030094+eraj14@users.noreply.github.com>
2024-03-12 23:55:33 -0400

    Test cases Eraj

--- src/components/RequestPage.test.jsx
+++ src/components/RequestPage.test.jsx
@@ -15,4 +15,18 @@ describe('Request Page', () => {
     fireEvent.change(input, { target: { value: 'P0150' } });
     expect(input.value).toBe('P0150');
   });
+  test("When a user chooses to repair, they can input a mileage value", () => {
+    render(<Request />);
+    expect(screen.getByText('Mileage:')).toBeTruthy();
+    const input = screen.getByPlaceholderText('Enter mileage');
+    fireEvent.change(input, { target: { value: '50000' } });
+    expect(input.value).toBe('50000');
+  });
+  test("User can click on DTC help which opens a popup with guiding information", () => {
+    render(<Request />);
+    const helpButton = screen.getByText('How to retrieve DTC Code');
+    fireEvent.click(helpButton);
+    const popupContent = screen.getByText('*Enter currently supported DTC code: P0150');
+    expect(popupContent).toBeTruthy();
+  });
 });
