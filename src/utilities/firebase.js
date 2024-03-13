2c3180b14678408b6ff04b2fb67ab9736b8aa5aa
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2024-03-13 11:35:43 -0500

    Delete Firebase config

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -8,15 +8,10 @@ import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signO
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAoG98kstplxRfHSn9DuTlZAjJZqRGjGdU",
-  authDomain: "obd-app-acded.firebaseapp.com",
-  projectId: "obd-app-acded",
-  storageBucket: "obd-app-acded.appspot.com",
-  messagingSenderId: "433829993281",
-  appId: "1:433829993281:web:19b06a3d30e27631285fc0",
-  measurementId: "G-WSSTWZSKS3"
-};
+
+// const firebaseConfig = {
+
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
